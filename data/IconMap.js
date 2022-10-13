2691626724657d5d49dba3d5703cda11d874e722
jessej-samuel <1234jessej@gmail.com>
2022-10-13 22:08:08 +0530

    fix: updated description, added snow bg

--- data/IconMap.js
+++ data/IconMap.js
@@ -2,13 +2,14 @@ import brokenClouds from "../public/assets/broken-clouds.jpg";
 import rain from "../public/assets/rain.gif"; // https://medium.com/retronator-magazine/down-the-color-wheel-with-merrigo-226ad1be0623
 import clearSky from "../public/assets/clear-sky.jpg";
 import thunderstorm from "../public/assets/thunderstorm.gif";
+import snow from "../public/assets/snow.gif";
 
 const Map = {
   clouds: {img: brokenClouds, icon: "☁️"},
   rain: {img: rain,icon: "🌧️"},
   thunderstorm: {img: thunderstorm, icon: "⛈️"},
   clear: {img: clearSky, icon: "☀️"},
-  snow: {img: rain, icon: "❄️"},
+  snow: {img: snow, icon: "❄️"}, // TODO: Find new snow bg
   mist: {img: rain, icon: "🌫️"},
   haze: {img: rain, icon: "🌫️"},
   drizzle: {img: rain, icon: "🌧️"},
